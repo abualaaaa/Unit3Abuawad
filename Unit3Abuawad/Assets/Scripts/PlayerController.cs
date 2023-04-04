522c92747b9fa12a1a7a7fbbd1d9eeee94465241
Abuawad <746951@student.dusd.net>
2023-04-04 13:14:53 -0700

    new
    new

--- Unit3Abuawad/Assets/Scripts/PlayerController.cs
+++ Unit3Abuawad/Assets/Scripts/PlayerController.cs
@@ -15,6 +15,9 @@ public class PlayerController : MonoBehaviour
     public AudioClip jumpSound;
     public AudioClip crashSound;
     private AudioSource playerAudio;
+    public bool doubleJumpUsed = false;
+    public float doubleJumpForce;
+    public bool doubleSpeed = false;
 
     // Start is called before the first frame update
     void Start()
@@ -35,25 +38,53 @@ public class PlayerController : MonoBehaviour
             playerAnim.SetTrigger("Jump_trig");
             dirtParticle.Stop();
             playerAudio.PlayOneShot(jumpSound, 1.0f);
-        }
-    }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            isOnGround = true;
-            dirtParticle.Play();
+            doubleJumpUsed = false;
         }
-        else if (collision.gameObject.CompareTag("Obstacle"))
+        else if (Input.GetKeyDown(KeyCode.Space) && !isOnGround && !doubleJumpUsed)
         {
-            gameOver = true;
-            Debug.Log("Game Over!");
-            playerAnim.SetBool("Death_b", true);
-            playerAnim.SetInteger("DeathType_int", 1);
-            explosionParticle.Play();
-            dirtParticle.Stop();
-            playerAudio.PlayOneShot(crashSound, 1.0f);
+            doubleJumpUsed = true;
+            playerRb.AddForce(Vector3.up * doubleJumpForce, ForceMode.Impulse);
+            playerAnim.Play("Running_Jump", 3, 0f);
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
+            {
+                if (Input.GetKey(KeyCode.LeftShift))
+                {
+                    doubleSpeed = true;
+                    playerAnim.SetFloat("Speed_Multiplier", 2.0f);
+                }
+                else if (doubleSpeed)
+                {
+                    doubleSpeed = false;
+                    playerAnim.SetFloat("Speed_Multiplier", 1.0f);
+                }
+
+
+
+
+
+                void OnCollisionEnter(Collision collision)
+                {
+                    if (collision.gameObject.CompareTag("Ground"))
+                    {
+                        isOnGround = true;
+                        dirtParticle.Play();
+                    }
+                    else if (collision.gameObject.CompareTag("Obstacle"))
+                    {
+                        gameOver = true;
+                        Debug.Log("Game Over!");
+                        playerAnim.SetBool("Death_b", true);
+                        playerAnim.SetInteger("DeathType_int", 1);
+                        explosionParticle.Play();
+                        dirtParticle.Stop();
+                        playerAudio.PlayOneShot(crashSound, 1.0f);
+                    }
+                }
+            }
         }
     }
 }
+
+
+              
